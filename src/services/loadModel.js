94964c1db1ddcdca9b646a16d4dc682ec15e49fc
ArmondHarer <armondharer@gmail.com>
2024-05-24 09:46:57 +0700

    change bucket link

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,7 +1,7 @@
 const tf = require("@tensorflow/tfjs-node");
 async function loadModel() {
   return tf.loadGraphModel(
-    "https://storage.googleapis.com/dicoding-mlgc-armondharer/model-in-prod/model.json"
+    "https://storage.googleapis.com/bucket-mlgc-armondharer/model.json"
   );
 }
 module.exports = loadModel;
